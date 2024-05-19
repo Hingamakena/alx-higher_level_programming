db596526ce13caf28b663ab1bcefaaf5f3b8dd91
hingamakena <hingamakena@gmail.com>
2024-05-19 02:18:19 -0700

    base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" first class Base """
+
+
+class Base:
+
+    """ private attribute of class """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is None:
+            self.id = id
+        else
+        Base.__nb_bojects += 1
+        self.id = Base._nb_objects
