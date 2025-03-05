f907b88a33751dd03d4da91eb9a7757931b15384
Hingamakena <hingamakena@gmail.com>
2025-03-05 07:16:04 -0800

    replace an element in list at a specific position without modifying the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    newlist = my_list[:]
+    for i in range(len(newlist)):
+        if my_list[idx] > len(my_list):
+            return new_list
+        if my_list[idx] < 0:
+            return newlist
+    
+    newlist[idx] = element
+
+    return newlist
