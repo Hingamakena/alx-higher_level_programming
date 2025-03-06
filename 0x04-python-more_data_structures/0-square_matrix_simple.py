4010620a58277218764ebbfb341121fb882790fa
Hingamakena <hingamakena@gmail.com>
2025-03-06 11:18:34 -0800

    attempt to square matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""squares value of all integers matris """
+def square_matrix_simple(matrix=[]):
+    for i in matrix:
+        for j in matrix:
+            return matrix[i**2]
