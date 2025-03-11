74a0a4e1bfb235c9ab00515d030775545d29b492
Hingamakena <hingamakena@gmail.com>
2025-03-11 01:07:42 -0700

    changed the logic flow of the code

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,11 +2,11 @@
 
 def new_in_list(my_list, idx, element):
     newlist = my_list[:]
-    for i in range(len(newlist)):
-        if my_list[idx] >= len(my_list):
-            return new_list
-        if my_list[idx] < 0:
-            return newlist
+
+    if idx >= len(my_list):
+        return my_list
+    if idx < 0:
+        return newlist
 
     newlist[idx] = element
 
