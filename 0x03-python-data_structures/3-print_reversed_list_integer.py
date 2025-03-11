3782a64c08fa6bcdc4a9f449b26d476cc7508c14
Hingamakena <hingamakena@gmail.com>
2025-03-11 00:51:54 -0700

    reverse then print the components

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    new_list = reverse(my_list)
-    print(new_list)
+    my_list.reverse()
+    for i in range(0, len(my_list)):
+        print("{}".format(my_list[i]))
