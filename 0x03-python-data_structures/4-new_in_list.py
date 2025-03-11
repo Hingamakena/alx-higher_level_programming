1eb7f19e749b7633dbe54d7e2644833140c62454
Hingamakena <hingamakena@gmail.com>
2025-03-11 01:05:30 -0700

    if >=

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 def new_in_list(my_list, idx, element):
     newlist = my_list[:]
     for i in range(len(newlist)):
-        if my_list[idx] > len(my_list):
+        if my_list[idx] >= len(my_list):
             return new_list
         if my_list[idx] < 0:
             return newlist
