ee0954210152e37b096b7055e0c1dfd211eb0303
Hingamakena <hingamakena@gmail.com>
2025-03-11 00:58:39 -0700

    changed the mode of reversing to listname[::-1]

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for i in range(0, len(my_list)):
-        print("{:d}".format(my_list[i]))
+    new_list = my_list[::-1]
+    for i in range(0, len(new_list)):
+        print("{:d}".format(new_list[i]))
