a89783ebf68fe9419d58aedae187c94522463e9b
Hingamakena <hingamakena@gmail.com>
2025-03-13 13:24:20 -0700

    loop list ...

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+def max_integer(my_list=[]):
+    x = 0
+    for i in range(len(my_list)):
+        if my_list[i] > my_list[i+1]:
+            x = my_list[i]
+        else:
+            x = my_list[i+1]
+
+    return x
