2417bebaf38d7be6f737ccb84ae3178139cae97c
Hingamakena <hingamakena@gmail.com>
2025-03-13 08:34:00 -0700

    class defining __size, area(), with try isinstance(int, value)

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+        try:
+            isinstance(int, size)
+        except TypeError:
+            print("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        def area(self):
+            return self.__size * 2
