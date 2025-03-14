2bbdcb17829f94a4118ad22d0d3c5bebdbe7908d
Hingamakena <hingamakena@gmail.com>
2025-03-14 05:23:21 -0700

    well documentd module

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+""" class that defines a square"""
+
 
 class Square:
     """ private instance size
         size method to set it """
     def __init__(self, size=0):
         self.__size = size
-
+    """ getter method"""
     def size(self):
         return self.__size
 
+    """setter method """
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size myst be an integer")
@@ -16,9 +19,11 @@ class Square:
             raise ValueError("size myst be >= 0")
         self.__size = value
 
+    """ returns the area of the square"""
     def area(self):
         return self.__size * self.__size
 
+    """prints the square"""
     def my_print(self):
         for i in range(self.__size):
             for j in range(self.__size):
