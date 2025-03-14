65e9e6f73773b9848cdfa9e72a2fc7eaa97b924c
Hingamakena <hingamakena@gmail.com>
2025-03-14 02:08:32 -0700

    int(size) > 0

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,6 +13,6 @@ class Square:
         except TypeError:
             print("size must be an integer")
         try:
-            size > 0 and instance(size, int)
+            int(size) > 0
         except ValueError:
             print("size must be >= 0")
