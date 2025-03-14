d51ca172d14f2c33ab251a703c21a78377e8793b
Hingamakena <hingamakena@gmail.com>
2025-03-14 02:35:43 -0700

    defines a method, corrected if not isinstance

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 
+""" class square that defines a square """
+
+
 class Square:
+    """ private instance size """
+
     def __init__(self, size=0):
-        self.__size = size
-        try:
-            isinstance(int, size)
-        except TypeError:
-            print("size must be an integer")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
 
         if size < 0:
             raise ValueError("size must be >= 0")
 
-        def area(self):
-            return self.__size * 2
+        """ defines the area of the square """
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
