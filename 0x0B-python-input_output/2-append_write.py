71be7bba0a606feb591042155cc386a35e3e9492
Hingamakena <hingamakena@gmail.com>
2025-03-15 13:03:57 -0700

    append() ?

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" function appends into a filename the text"""
+
+
+def append_write(filename="", text=""):
+    """ using with to open filename """
+
+    with open(filename, "a") as filewhenopen:
+        filewhenopen.append(text)
