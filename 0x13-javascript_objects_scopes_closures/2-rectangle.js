3f443e6d48091a2b1e598899e9ae1348984fe258
Hingamakena <hingamakena@gmail.com>
2025-03-16 23:32:58 -0700

    attempt to create a rectangle class with javascript

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+class Rectangle{
+  constructor(w, h){
+	if w <= 0:
+	  this.width = undefined
+	if h <= 0:
+	  this width = undefined
+	this.width = w;
+	this.height = h;
+  }
+}
---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+class Rectangle{
+  constructor(w, h){
+	  this.widht = w;
+	  this.height = h;
+  }
+}
