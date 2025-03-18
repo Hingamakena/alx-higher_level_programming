a7a3342fa95da0dac217adc74292886fad5b719b
Hingamakena <hingamakena@gmail.com>
2025-03-18 04:55:12 -0700

    replace an element in occurence. - failed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,9 +2,9 @@
 
 """ replace all occurences of an element by another in new list"""
 def search_replace(my_list, search, replace):
-    newlist = my_list[:]
+    new_list = my_list[:]
 
-    for i in range(0, len(my_list)):
-        if search in my_list:
-            newlist[search] = replace
-    return newlist
+    for i in range(0, len(new_list)):
+        if search in new_list:
+            new_list[search] = replace
+    return new_list
