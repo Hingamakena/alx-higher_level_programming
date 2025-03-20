546d757ae03c9fcdde80a7d7614e9c513ad0a71a
Hingamakena <hingamakena@gmail.com>
2025-03-19 23:15:51 -0700

    if isinstance of the specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" check if an obj is an instance of the specified class """
+
+
+def is_same_class(obj, a_class):
+    """ if isinstance return True, else False"""
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
