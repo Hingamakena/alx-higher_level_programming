ff52c20748bd5114afa2a38269dc750e2b8a8390
Hingamakena <hingamakena@gmail.com>
2025-03-20 13:46:42 -0700

    len of list -1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,7 +4,7 @@
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
 
-    for i in range(0, len(new_list)):
+    for i in range(0, len(new_list) - 1):
         if search in new_list:
             new_list[search] = replace
     return new_list
