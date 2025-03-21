09b5df8e309b80aa0b98baf07b252f829d0515ad
Hingamakena <hingamakena@gmail.com>
2025-03-20 22:57:07 -0700

    no permissions

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@
 def read_file(filename=""):
     """ reads a test file(utf*) mode and print to stdout"""
 
-    with open(filename, "r", encoding="utf-8") as file_handler:
+    with open(filename,  encoding="utf-8") as file_handler:
         read_data = file_handler.read()
         print(read_data)
