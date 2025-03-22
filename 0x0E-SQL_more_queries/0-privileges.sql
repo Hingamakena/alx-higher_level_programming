eea1360f8f9ca7c45767e7cffecb0324c5501f2e
Hingamakena <hingamakena@gmail.com>
2025-03-21 22:23:14 -0700

    users separated by comma

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- list privileges of MySWl users user_od_1 and user_0d_2
 
-SHOW GRANTS FOR 'user_0d_1' AND 'user_0d_2'@'locahhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'locahhost';
