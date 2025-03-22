a8053770f829e8d5c9df3242073a557a75cef583
Hingamakena <hingamakena@gmail.com>
2025-03-22 02:30:31 -0700

    find the max integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 
 def max_integer(my_list=[]):
     x = 0
-    for i in range(len(my_list)):
+    for i in range(0, len(my_list)):
         if my_list[i] > my_list[i+1]:
             x = my_list[i]
         else:
