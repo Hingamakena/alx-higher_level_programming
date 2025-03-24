ffe8cc485a87ad932bfdc8da9a85a29785b08622
Hingamakena <hingamakena@gmail.com>
2025-03-24 00:21:46 -0700

    reduced the number of file lines

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
-"""return a list qith all values multiplied by number with no loop"""
-
-
 def multiply_list_map(my_list=[], number=0):
     newlist = list(map(lambda x: (x * number), my_list))
     return newlist
