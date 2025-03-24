967e94d5f561f274a9ba87cd33ed5bba0b1d567c
Hingamakena <hingamakena@gmail.com>
2025-03-23 23:57:23 -0700

    documented the file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+""" class Square that defines a square """
+
 
 class Square:
+    """ self instantiation with size=0, position =(0,0)"""
     def __init__(self, size=0, position=(0,0)):
         self.__size = size
         self.__position = position
 
+    """ getter of size """
     def size(self):
         return self.__size
 
+    """ setter of size """
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -16,9 +21,11 @@ class Square:
 
         self.__size = value
 
+    """ getter of position """
     def position(self):
         return self.__position
 
+    """setter of position """
     def position(self, value):
         if not isinstance(value, tuple):
             raiseTypeError("position must be a tuple of 2 positive integers")
