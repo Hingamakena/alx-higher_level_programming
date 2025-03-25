b5b1599876bcd076088f9f5fbffd2446bb87913e
Hingamakena <hingamakena@gmail.com>
2025-03-25 04:32:18 -0700

    argv.forEach(){}. Method failed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const {argv} = require('node:process');
+
+for (let i =1; argv[i] != null; i++){
+	if (argv[i] <= 1){
+		console.log("No Argument");
+	}
+	else if (argv[i] > 1 && argv[i] < 3){
+		console.log("Argument found");
+	}
+	else
+		console.log("Arguments found");
+};
