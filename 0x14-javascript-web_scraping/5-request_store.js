72ffa5e8b4460fff8a3edd0f58999709a678aada
Hingamakena <hingamakena@gmail.com>
2025-03-26 06:01:08 -0700

    revisit

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+/* Script that gets the content of a webpage and stores it in file
+ *
+ * first argument - URL to the request
+ *
+ * Second argument - file path to store body response
+ *
+ * fule must be utf-8 encoded
+ *
+ * Must use the module request
+ *
+ * */
+
+const request = require('request');
+
+//
