18020c5b765bc075514a864294fbb6eeadfa7ea8
Hingamakena <hingamakena@gmail.com>
2025-03-28 01:22:45 -0700

    return elem & elem

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 """ functin that returns a set of common elements """
+
+
 def common_elements(set_1, set_2):
-    for i in range(0, len(set_1)):
-        for j in range(0, len(set_2)):
-            if set_1[i] == set_2[j]:
-                return i
-    return j
+    return set_1 & set_2
