03019605fff7b876c9a8e455eee58bc153ce4d4a
Hingamakena <hingamakena@gmail.com>
2025-03-29 02:41:23 -0700

    return max(my_list[i])

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python
 
 def max_integer(my_list=[]):
-    x = 0
-    for i in range(0, len(my_list)):
-        if my_list[i] > my_list[i+1]:
-            x = my_list[i]
-        else:
-            x = my_list[i+1]
-
-    return x
+    for i in my_list:
+        return max(my_list[i])
