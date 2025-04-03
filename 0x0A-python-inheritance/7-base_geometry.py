aaa8309e0f40c126f959a8628fe03cb4e82479fa
Hingamakena <hingamakena@gmail.com>
2025-04-03 09:24:39 -0700

    changed the format(value) to format(name)

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -13,10 +13,10 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(value))
+            raise TypeError("{} must be an integer".format(name))
 
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(value))
+            raise ValueError("{} must be greater than 0".format(name))
 
         self.name = name
         self. value = value
