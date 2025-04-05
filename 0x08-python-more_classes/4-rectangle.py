0f7bb1e4ebb0b5aac2a5e8fb2b03fd3201da60a4
Hingamakena <hingamakena@gmail.com>
2025-04-04 22:43:13 -0700

    task 4 rectangle

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -3,8 +3,9 @@
 
 
 class Rectangle:
-    self.__width=0
-    
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
     """ property to retrieve width """
     def width(self):
         return self.__width
@@ -13,3 +14,26 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__width = value
+
+    def height(self):
+        return self.__height
+
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        return ((self.__height * 2) + (self.__width * 2))
