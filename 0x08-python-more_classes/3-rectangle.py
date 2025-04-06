3530478c9196d39ff7bd98cdc8c437dbdd1b6a80
Hingamakena <hingamakena@gmail.com>
2025-04-05 22:04:53 -0700

    added the property setters and the property method widht

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -9,7 +9,11 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
-    """ set width value """
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
     def width(self, value):
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
@@ -19,9 +23,11 @@ class Rectangle:
 
         self.__width = value
 
+    @property
     def height(self):
         return self.__height
 
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
@@ -35,4 +41,4 @@ class Rectangle:
         return self.__height * self.__width
 
     def perimeter(self):
-        return ((self.__height * 2 ) + (self.__width * 2))
+        return ((self.__height * 2) + (self.__width * 2))
