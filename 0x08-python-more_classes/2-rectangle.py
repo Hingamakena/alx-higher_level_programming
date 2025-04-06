d5e23b799fd462ca22b75f3dcacd0974d06cc3c7
Hingamakena <hingamakena@gmail.com>
2025-04-05 22:12:16 -0700

    documented the class and pycoded it

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -4,14 +4,18 @@
 
 
 class Rectangle:
+
+    """ Instantiation with width and height """
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
 
+    """ property getter of width"""
     @property
     def width(self):
         return self.__width
 
+    """ property setter of width """
     @width.setter
     def width(self, value):
         if not isinstance(width, int):
@@ -22,10 +26,12 @@ class Rectangle:
 
         self.__width = value
 
+    """ property getter of height """
     @property
     def height(self):
         return self.__height
 
+    """ property setter of height """
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
