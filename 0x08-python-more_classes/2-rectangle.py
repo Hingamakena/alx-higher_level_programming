d04d52900cb5cdf158dfc17cd9c5d87b847bda24
Hingamakena <hingamakena@gmail.com>
2025-04-05 22:15:25 -0700

    from if width< 0 to if value< 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("Width must be an integer")
 
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
 
         self.__width = value
