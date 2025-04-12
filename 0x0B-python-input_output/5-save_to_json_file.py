c612a9236362951d06081dab0f7638d0676dd0e4
Hingamakena <hingamakena@gmail.com>
2025-04-12 13:04:27 -0700

    documents the file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+
+""" imports a son module """
 import json
 
 """ saves a json object in a file"""
 
-
 def save_to_json_file(my_obj, filename):
+
     """ opens a file with name filename """
 
     with open(filename, encoding="utf-8") as f:
         write_data = f.write(json.dumps(my_obj))
 
+    """ returns written data holder"""
     return write_data
