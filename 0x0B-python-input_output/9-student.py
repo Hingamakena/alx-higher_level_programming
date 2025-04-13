cc41e8a992dd3df088bcf6df2d9193ff4a78f838
Hingamakena <hingamakena@gmail.com>
2025-04-13 13:02:51 -0700

    pycoded the code

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ class Student defining a student """
 
+
 class Student:
     """ instantiation with first_name and last_name """
 
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
-        self.last_name= last_name
+        self.last_name = last_name
         self.age = age
     """ method retrieves a dictionary representatin of a student """
 
