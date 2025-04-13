86304ddb9caef44f0ea24b456ac8d45d23bd0ca7
Hingamakena <hingamakena@gmail.com>
2025-04-13 12:13:08 -0700

    initialized the variables with method init

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,11 +3,10 @@ from base import Base
 
 """ class rectangle inheriting from Base"""
 
-
 class Rectangle(Base):
-    pass
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("test_rectangle.txt")
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().id
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
