2a51d2de72cd9744a64f3495ecfd9584a96425b4
Hingamakena <hingamakena@gmail.com>
2025-04-13 12:17:51 -0700

    documented class rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,10 @@ from base import Base
 """ class rectangle inheriting from Base"""
 
 class Rectangle(Base):
+    """ class constructor """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ calls the super class with id """
+
         super().id
         self.__width = width
         self.__height = height
