617358c8c895ae5b9ffa7f7d44af1dcbb8d99cd9
Hingamakena <hingamakena@gmail.com>
2025-04-13 13:01:28 -0700

    well documented module

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+""" class Student defining a student """
 
 class Student:
+    """ instantiation with first_name and last_name """
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name= last_name
         self.age = age
+    """ method retrieves a dictionary representatin of a student """
 
     def to_json(self):
         return self.__dict__
