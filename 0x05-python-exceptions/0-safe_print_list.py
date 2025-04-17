d1bc8fc371afbca58d076002776cef4df6f62501
Hingamakena <hingamakena@gmail.com>
2025-04-16 21:55:52 -0700

    attempt to safely pring integers in a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range(0,x):
-            print("{}".format(my_list[i]))
+        for i in range(0, my_list[x]):
+            print("{:d}".format(my_list[i]))
     except TypeError as e:
-        return None
+        print("string format not supported")
