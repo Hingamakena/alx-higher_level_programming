b8c67aa0e15ae3c34a3faca66e8b2ef6dbd4eb68
Hingamakena <hingamakena@gmail.com>
2025-04-17 21:44:55 -0700

    documented the _init method

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-""" class defines a rectabgle """
+
+""" class defines a rectangle """
 
 
 class Rectangle:
+    """ instantiation with width and height """
+
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
