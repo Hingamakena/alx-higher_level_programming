b4438015cd322d22f4ba0528ac53587464cf980c
Hingamakena <hingamakena@gmail.com>
2025-04-20 22:15:59 -0700

    initialized the super id attr and documented the method __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,20 +4,16 @@ from models.base import Base
 
 """ class rectangle inheriting from Base"""
 
+
 class Rectangle(Base):
 
-    """ class constructor """
-
+    """ class constructor with attributes width, height, x, y """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ calls the super class with id """
-
-        """ rest of the locally defined attributes """
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        """ inherit from super class id """
-
     """
     getter and setter excluding id """
 
@@ -30,4 +26,6 @@ class Rectangle(Base):
             for j in range(0,self.__width):
                 print('#',end="")
             print('#')
-
+    """ return the string rep of the class """
+    def __str__(self):
+        return f"{self.__class__.__name__} ({self.id}) {self.__x}/{self.__y} {self.__width}/{self.__height}"
