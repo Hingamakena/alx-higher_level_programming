0a1dd564801985923e82d4c750d4e6d3379b8a5b
Hingamakena <hingamakena@gmail.com>
2025-04-20 22:19:45 -0700

    well documented the module

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,25 +7,12 @@ from models.base import Base
 
 class Rectangle(Base):
 
-    """ class constructor with attributes width, height, x, y """
+    """ class constructor with the class trributes 
+    calls the super class with id using the logic of __init__"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-    """
-    getter and setter excluding id """
-
-    """ method area returning the area of the rectangle """
-    def area(self):
-        return self.__width * self.__height
-
-    def display(self):
-        for i in range(0, self.__height):
-            for j in range(0,self.__width):
-                print('#',end="")
-            print('#')
-    """ return the string rep of the class """
-    def __str__(self):
-        return f"{self.__class__.__name__} ({self.id}) {self.__x}/{self.__y} {self.__width}/{self.__height}"
