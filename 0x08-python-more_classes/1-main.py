8ccccb6db35b414df716e8830fa69fead7325a69
Hingamakena <hingamakena@gmail.com>
2025-04-23 14:27:26 -0700

    main test files

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -8,3 +8,12 @@ my_rectangle.width = 10
 my_rectangle.height = 3
 print(my_rectangle.__dict__)
 
+try:
+    my_rectangle = Rectangle.width = -12
+except Exception as e:
+    print(e)
+
+try:
+    my_rectangle = Rectangle.height = -23
+except Exception as e:
+    print(e)
