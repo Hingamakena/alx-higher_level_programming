fa2c1f6e9f8aa63cdaec4f3d90f51fdf4586449b
Hingamakena <hingamakena@gmail.com>
2025-04-23 12:27:53 -0700

    defines the methods without the property setter method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,43 +1,32 @@
 #!/usr/bin/python3
 
-"""updated class Rectangle"""
+""" class Rectangle that defines a rectangle on width and height"""
 
 
 class Rectangle:
-    """ initialization with height and width as private """
-
-    def __init__(self, height=0, width=0):
+    """ instantiation with optional width and height """
+    def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
 
-    """ property getter of width"""
-    @property
+    """ retrieve the width value """
     def width(self):
         return self.__width
 
-    """ property setter of width """
-    @width.setter
+    """ set the value of width """
     def width(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an int")
-
+            raise TypeError("width must be bean integer")
         if value < 0:
             raise ValueError("width must be >= 0")
 
-        self.__width = value
-
-    """ property getter of height """
-    @property
+    """ retrieve the height value """
     def height(self):
         return self.__height
 
     """ property setter of height"""
-    @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("height must be an int")
-
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-
-        self.__height = value
