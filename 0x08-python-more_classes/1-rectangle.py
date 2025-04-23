a94ec0912989cded06185e82ad927347db983130
Hingamakena <hingamakena@gmail.com>
2025-04-23 13:48:17 -0700

    contains property decorator

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,23 +10,29 @@ class Rectangle:
         self.__height = height
 
     """ retrieve the width value """
+    @property
     def width(self):
         return self.__width
 
     """ set the value of width """
+    @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be bean integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     """ retrieve the height value """
+    @property
     def height(self):
         return self.__height
 
     """ property setter of height"""
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
