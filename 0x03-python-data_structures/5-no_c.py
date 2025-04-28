10c901e28c70852d51de6389fba95f788bc09d5c
Hingamakena <hingamakena@gmail.com>
2025-04-28 13:09:47 -0700

    Error: str obj does not support item assignment

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_string = list(my_string)
-    for i in range(0, len(new_string)):
-        if 'c' in new_string:
-            new_string.remove(i)
-        if 'C' in new_string:
-            new_string.remove(i)
-    return str(new_string)
+    for i in my_string:
+        if 'c' in my_string:
+            my_string[i] = 'f'
+        i += 1
+    return my_string
