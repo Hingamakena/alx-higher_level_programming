da88e8b3e7e3a849fa8565459b8b090d7143632c
Hingamakena <hingamakena@gmail.com>
2025-04-29 09:29:54 -0700

    read data from a file and print int

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
-""" read file, filename as argument"""
+""" read a text and prints it to stdout"""
 
 
 def read_file(filename=""):
-    """ reads a test file(utf*) mode and print to stdout"""
-
-    with open(filename,  encoding="utf-8") as file_handler:
-        read_data = file_handler.read()
-        print("{}".format(read_data))
-
-        return read_data
+    with open(filename, "r") as f:
+        data = f.read()
+    print(data, end="")
