115209894d5dd7a33731babd45a365dcff79aebe
hingamakena <hingamakena@gmail.com>
2025-04-30 21:56:53 +0000

    base model of base file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,18 +1,1 @@
 #!/usr/bin/python3
-""" imports a module from the base module file """
-from models.base import Base
-
-""" class rectangle inheriting from Base"""
-
-
-class Rectangle(Base):
-
-    """ class constructor with the class trributes 
-    calls the super class with id using the logic of __init__"""
-
-    def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
