a39387d1a5c7574c0f9f1789c01eac33ac49be06
hingamakena <hingamakena@gmail.com>
2025-04-30 21:17:12 +0000

    initial trial and attempt

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ import the unittest module """
 import unittest
+from models import base
+from models.base import Base
 
 """ subclass of the testing class """
-class MyUnitTesting(unittest.Unittest):
-
+class MyUnitTesting(unittest.TestCase):
+	cl_1 = Base(1)
+	self.assertEqual(cl_1.id, 1)
