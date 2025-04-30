0879c39f2ea50a752363622bf1427f99bd8be509
hingamakena <hingamakena@gmail.com>
2025-04-30 22:03:01 +0000

    class rectangle with class initializer

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+""" class rectangle that inherits from Base """
+
+
+class Rectangle(Base):
+	""" class constructor """
+	def __init__(self, width, height, x=0, y=0, id=None):
+		self.__width = width
+		self.__height = height
+		self.__x = x
+		self.__y = y
+		super().__init__(id)
