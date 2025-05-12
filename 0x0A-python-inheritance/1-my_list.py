f18632b125f87fd62a66786778eb1031858e974f
Hingamakena <hingamakena@gmail.com>
2025-05-12 12:36:06 -0700

    inherited list property

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 """ class list inherits from list """
+
+
 class MyList(list):
     """print the list, but sorted"""
 
     def print_sorted(self):
-        return sorted(self)
+        print(sorted(self))
+
 
 if __name__ == "__main__":
     import doctest
