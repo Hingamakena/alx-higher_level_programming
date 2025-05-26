9a09c445bc656e09ebeacc6b372f3dc8be0f9aca
Hingamakena <hingamakena@gmail.com>
2025-05-26 08:06:37 -0700

    if my_list:

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,7 @@
 def print_reversed_list_integer(my_list=[]):
     """ print all integers of a list, reversed """
 
-    if isinstance(my_list, list):
+    if my_list:
         my_list.reverse()
         for i in range(0, len(my_list)):
             print("{:d}".format(my_list[i]))
