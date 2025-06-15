42810a354807c6845f502e66d2956d370f59786e
Hingamakena <hingamakena@gmail.com>
2025-06-14 22:09:01 -0700

    added height and with setter and getter, documented the id inheritance

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+from base import Base
 """ class Rectangle inherits from Base """
 
 
@@ -11,7 +11,32 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super.__init__(id)
+
+        """ id is inherited from the baseclass """
+
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            print("value is not an integer")
+
+        self.__height = value
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            print("value is not an integer")
+
+        self.__width = value
