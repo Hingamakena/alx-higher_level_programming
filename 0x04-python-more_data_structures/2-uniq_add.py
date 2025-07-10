78c0e89c64a34025d4a6f366d2bc542387629f1a
hingamakena <hingamakena@gmail.com>
2025-07-10 00:44:39 -0700

    adds unique numbers using set

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,8 +2,7 @@
 """ add all unique integers in a list """
 
 def uniq_add(my_list=[]):
-    for i in range(len(my_list)):
-        for j in range(len(my_list[:])):
-            if i != j:
-                sum = +1
-
+    sum = 0
+    for i in set(my_list):
+        sum += i
+    return sum
